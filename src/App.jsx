cc977c535fcc5b616c89e196510378a726c7b51e
shearichard <richard.shea@nec.co.nz>
2022-04-06 11:44:03 +1200

    Starting point for sse implementation

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h1>React/server-sent-events Sandbox</h1>
+        <h2>The SSE stuff</h2>
+        <p>
+            A placeholder for the SSE stuff.
+        </p>
+        <h2>The non-SSE stuff</h2>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
