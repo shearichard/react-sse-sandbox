03731eb8887832f3b3209f48dafdafd9644580a2
shearichard <richard.shea@nec.co.nz>
2022-04-06 17:30:33 +1200

    This fails on CORS errors but I'm committing it anyway for future reference

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import axios from 'axios'
 
 class AppWrapper extends React.Component {
+    //--------------------------------------------------------------------
 	constructor(props){
 		super(props)
 		console.log("AppWrapper constructor firing")
+        this.eventSource = new EventSource("http://localhost:8080/product_request_receiver");
+		console.log("AppWrapper constructor eventSource Initialized")
 	}
-
+    //--------------------------------------------------------------------
 	componentDidMount() {
 		console.log("AppWrapper componentDidMount firing")
-	}
+        this.eventSource.addEventListener("dataUpdate", e =>
+            this.updateState(JSON.parse(e.data))
+        );
 
+        axios.get("http://localhost:8080",
+            {headers: {'Access-Control-Allow-Origin': '*'}
+        })
+        .then(
+            (result) => {
+                console.log("OK. result.data follows ...")
+                console.log(result.data)
+            },
+            (error) => {
+                console.log("NOT OK. error follows ...")
+                console.log(error)
+            }
+            /*
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    data: result.data
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+            */
+        )
+	}
+    //--------------------------------------------------------------------
 	render() {
 		return (
 			<>
